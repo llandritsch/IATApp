8047b658aa5902c0c9d1acfcc1fc1309e717ffac
Lisa Andritsch <lisa3718@gmail.com>
2020-09-25 06:02:43 -0500

    completed JS email validation file

--- validation.js
+++ validation.js
@@ -1,8 +1,12 @@
 
 const validateForm = () => {
-    let firstname = document.forms["createAccount"]["firstName"].value;
-    if (firstname.length <= 5) {
+    let input = document.querySelector("#email");
+    let email = input.value;
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
         alert("Please add your first name");
+        return true;
+    } else {
+        alert("Please enter a valid email address");
         return false;
     }
 }
